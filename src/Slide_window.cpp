38dd58a58a9c3beca3bdfe82ec43fd829552e833
Jiangqiu Shen <ivy22233qiu@live.com>
2021-01-09 19:37:33 -0500

    add iterator for multilevel

--- src/Slide_window.cpp
+++ src/Slide_window.cpp
@@ -4,153 +4,135 @@
 
 #include "Slide_window.h"
 
-#include <utility>
-#include <map>
-#include <numeric>
+#include "size.h"
 #include <algorithm>
 #include <cassert>
-#include "size.h"
+#include <map>
+#include <numeric>
+#include <utility>
 
-Slide_window::Slide_window(int x, int y, int xw, int yw, int level, uint64_t inputAddr, uint64_t edgeAddr,
-                           uint64_t outputAddr, int inputLen, int edgeLen, int outputLen, int numNodesInWindow,
+Slide_window::Slide_window(int x, int y, int xw, int yw, int level,
+                           uint64_t inputAddr, uint64_t edgeAddr,
+                           uint64_t outputAddr, int inputLen, int edgeLen,
+                           int outputLen, int numNodesInWindow,
                            int currentNodeSize)
-        : x(x), y(y), xw(xw), yw(yw),
-          level(level),
-          input_addr(inputAddr),
-          edge_addr(edgeAddr),
-          output_addr(outputAddr),
-          input_len(inputLen),
-          edge_len(edgeLen),
-          output_len(outputLen), num_nodes_in_window(numNodesInWindow), current_node_size(currentNodeSize) {}
+    : x(x), y(y), xw(xw), yw(yw), level(level), input_addr(inputAddr),
+      edge_addr(edgeAddr), output_addr(outputAddr), input_len(inputLen),
+      edge_len(edgeLen), output_len(outputLen),
+      num_nodes_in_window(numNodesInWindow),
+      current_node_size(currentNodeSize) {}
 
-int Slide_window::getX() const {
-    return x;
-}
+int Slide_window::getX() const { return x; }
 
-int Slide_window::getY() const {
-    return y;
-}
+int Slide_window::getY() const { return y; }
 
-int Slide_window::getXw() const {
-    return xw;
-}
+int Slide_window::getXw() const { return xw; }
 
-int Slide_window::getYw() const {
-    return yw;
-}
+int Slide_window::getYw() const { return yw; }
 
-int Slide_window::getLevel() const {
-    return level;
-}
+int Slide_window::getLevel() const { return level; }
 
-uint64_t Slide_window::getInputAddr() const {
-    return input_addr;
-}
+uint64_t Slide_window::getInputAddr() const { return input_addr; }
 
-uint64_t Slide_window::getEdgeAddr() const {
-    return edge_addr;
-}
+uint64_t Slide_window::getEdgeAddr() const { return edge_addr; }
 
-uint64_t Slide_window::getOutputAddr() const {
-    return output_addr;
-}
+uint64_t Slide_window::getOutputAddr() const { return output_addr; }
 
-int Slide_window::getInputLen() const {
-    return input_len;
-}
+int Slide_window::getInputLen() const { return input_len; }
 
-int Slide_window::getEdgeLen() const {
-    return edge_len;
-}
+int Slide_window::getEdgeLen() const { return edge_len; }
 
-int Slide_window::getOutputLen() const {
-    return output_len;
-}
+int Slide_window::getOutputLen() const { return output_len; }
 
-int Slide_window::getNumNodesInWindow() const {
-    return num_nodes_in_window;
-}
+int Slide_window::getNumNodesInWindow() const { return num_nodes_in_window; }
 
-int Slide_window::getCurrentNodeSize() const {
-    return current_node_size;
-}
+int Slide_window::getCurrentNodeSize() const { return current_node_size; }
 
-Slide_window_set::Slide_window_set(std::shared_ptr<Graph> mGraph, std::vector<int> xwS,
-                                   std::vector<int> ywS, std::vector<int> nodeSizeS, int totalLevel)
-        : m_graph(std::move(mGraph)), xw_s(std::move(xwS)), yw_s(std::move(ywS)), node_size_s(std::move(nodeSizeS)),
-          total_level(totalLevel) {
-    assert(xw_s.size() == total_level - 1);
-    assert(node_size_s.size() == total_level);
+Slide_window_set::Slide_window_set(std::shared_ptr<Graph> mGraph,
+                                   std::vector<int> xwS, std::vector<int> ywS,
+                                   std::vector<int> nodeSizeS, int totalLevel)
+    : m_graph(std::move(mGraph)), xw_s(std::move(xwS)), yw_s(std::move(ywS)),
+      node_size_s(std::move(nodeSizeS)), total_level(totalLevel) {
+  assert(xw_s.size() == total_level - 1);
+  assert(node_size_s.size() == total_level);
 
-    uint64_t start_addr = 0xff11ff11;
-    for (auto l = 0; l < totalLevel; l++) {
-        node_addrs.push_back(start_addr);
-        start_addr += m_graph->get_num_nodes() * node_size_s[l] * single_node_size;
-    }
+  uint64_t start_addr = 0xff11ff11;
+  for (auto l = 0; l < totalLevel; l++) {
+    node_addrs.push_back(start_addr);
+    start_addr += m_graph->get_num_nodes() * node_size_s[l] * single_node_size;
+  }
 
-    for (auto level_i = 0; level_i < totalLevel - 1; level_i++) {
-        m_sliding_window_multi_level.emplace_back();
-        auto col_i = 0;
-        while (col_i < m_graph->get_num_nodes()) {
-            m_sliding_window_multi_level[level_i].emplace_back();
-            std::map<int, int> row_to_count;
-            auto col_end = col_i + xw_s[level_i];
-            if (col_end > m_graph->get_num_nodes()) {
-                col_end = m_graph->get_num_nodes();
-            }
-            auto start_edge_index = m_graph->get_edge_index()[col_i];
-            auto end_edge_index = m_graph->get_edge_index()[col_end];
-            for (auto e_index = start_edge_index; e_index < end_edge_index; e_index++) {
-                row_to_count[m_graph->get_edges()[e_index]]++;
-            }
-            auto row_i = 0;
+  for (auto level_i = 0; level_i < totalLevel - 1; level_i++) {
+    m_sliding_window_multi_level.emplace_back();
+    auto col_i = 0;
+    while (col_i < m_graph->get_num_nodes()) {
+      m_sliding_window_multi_level[level_i].emplace_back();
+      std::map<int, int> row_to_count;
+      auto col_end = col_i + xw_s[level_i];
+      if (col_end > m_graph->get_num_nodes()) {
+        col_end = m_graph->get_num_nodes();
+      }
+      auto start_edge_index = m_graph->get_edge_index()[col_i];
+      auto end_edge_index = m_graph->get_edge_index()[col_end];
+      for (auto e_index = start_edge_index; e_index < end_edge_index;
+           e_index++) {
+        row_to_count[m_graph->get_edges()[e_index]]++;
+      }
+      auto row_i = 0;
 
-            while (row_i < m_graph->get_num_nodes()) {
-                while (row_i < m_graph->get_num_nodes() and !row_to_count.count(row_i)) {
-                    //empty line
-                    row_i++;
-                }
-                if (row_i >= m_graph->get_num_nodes()) {
-                    break;
-                }
-                auto row_end = row_i + yw_s[level_i];
-                if (row_end > m_graph->get_num_nodes()) {
-                    row_end = m_graph->get_num_nodes();
-                }
-                while (!row_to_count.count(row_end - 1)) {
-                    row_end--;
-                }
-                //now we build a window;
-                assert(row_end > row_i);
-                auto lower_bound = row_to_count.lower_bound(row_i);
-                auto upper_bound = row_to_count.lower_bound(row_end);
-                auto total_node = std::accumulate(lower_bound, upper_bound, 0, [](int value, auto &&pair) {
-                    return value + pair.second;
-                });
-                auto &&edge_index = m_graph->get_edge_index();
-
-                uint64_t input_addr = node_addrs.at(level_i) + row_i * node_size_s.at(level_i) * single_node_size;
-
-                uint64_t edge_addr = m_graph->get_edge_addr(edge_index.at(col_i));
-
-                uint64_t output_addr =
-                        node_addrs.at(level_i + 1) + col_i * node_size_s.at(level_i + 1) * single_node_size;
-                int input_len = (row_end - row_i) * node_size_s.at(level_i) * single_node_size;
-
-                auto edge_len = (edge_index.at(col_end) -
-                                 edge_index.at(col_i)) * 4;
-
-                int output_len = (col_end - col_i) * node_size_s.at(level_i + 1) * single_node_size;
-
-                m_sliding_window_vec.emplace_back(col_i, row_i, xw_s[level_i], row_end - row_i, level_i, input_addr,
-                                                  edge_addr,
-                                                  output_addr, input_len, edge_len, output_len, total_node,
-                                                  node_size_s[level_i]);
-
-                row_i = row_end;
-
-            }
-            col_i = col_end;
+      while (row_i < m_graph->get_num_nodes()) {
+        while (row_i < m_graph->get_num_nodes() and
+               !row_to_count.count(row_i)) {
+          // empty line
+          row_i++;
         }
+        if (row_i >= m_graph->get_num_nodes()) {
+          break;
+        }
+        auto row_end = row_i + yw_s[level_i];
+        if (row_end > m_graph->get_num_nodes()) {
+          row_end = m_graph->get_num_nodes();
+        }
+        while (!row_to_count.count(row_end - 1)) {
+          row_end--;
+        }
+        // now we build a window;
+        assert(row_end > row_i);
+        auto lower_bound = row_to_count.lower_bound(row_i);
+        auto upper_bound = row_to_count.lower_bound(row_end);
+        auto total_node = std::accumulate(
+            lower_bound, upper_bound, 0,
+            [](int value, auto &&pair) { return value + pair.second; });
+        auto &&edge_index = m_graph->get_edge_index();
+
+        uint64_t input_addr =
+            node_addrs.at(level_i) +
+            row_i * node_size_s.at(level_i) * single_node_size;
+
+        uint64_t edge_addr = m_graph->get_edge_addr(edge_index.at(col_i));
+
+        uint64_t output_addr =
+            node_addrs.at(level_i + 1) +
+            col_i * node_size_s.at(level_i + 1) * single_node_size;
+        int input_len =
+            (row_end - row_i) * node_size_s.at(level_i) * single_node_size;
+
+        auto edge_len = (edge_index.at(col_end) - edge_index.at(col_i)) * 4;
+
+        int output_len =
+            (col_end - col_i) * node_size_s.at(level_i + 1) * single_node_size;
+
+        m_sliding_window_vec.emplace_back(
+            col_i, row_i, xw_s[level_i], row_end - row_i, level_i, input_addr,
+            edge_addr, output_addr, input_len, edge_len, output_len, total_node,
+            node_size_s[level_i]);
+        m_sliding_window_multi_level.back().back().emplace_back(
+            col_i, row_i, xw_s[level_i], row_end - row_i, level_i, input_addr,
+            edge_addr, output_addr, input_len, edge_len, output_len, total_node,
+            node_size_s[level_i]);
+        row_i = row_end;
+      }
+      col_i = col_end;
     }
+  }
 }
--- src/Slide_window.h
+++ src/Slide_window.h
@@ -5,119 +5,124 @@
 #ifndef GCN_SIM_SLIDE_WINDOW_H
 #define GCN_SIM_SLIDE_WINDOW_H
 
-#include <iostream>
+#include "fmt/format.h"
 #include <algorithm>
 #include <graph.h>
+#include <iostream>
 #include <memory>
-#include "fmt/format.h"
 
 class Slide_window {
 public:
+  Slide_window() = default;
 
-    Slide_window() = default;
+  Slide_window(int x, int y, int xw, int yw, int level, uint64_t inputAddr,
+               uint64_t edgeAddr, uint64_t outputAddr, int inputLen,
+               int edgeLen, int outputLen, int numNodesInWindow,
+               int currentNodeSize);
 
-    Slide_window(int x, int y, int xw, int yw, int level, uint64_t inputAddr, uint64_t edgeAddr,
-                 uint64_t outputAddr, int inputLen, int edgeLen, int outputLen, int numNodesInWindow,
-                 int currentNodeSize);
+  [[nodiscard]] int getX() const;
 
-    [[nodiscard]] int getX() const;
+  [[nodiscard]] int getY() const;
 
-    [[nodiscard]] int getY() const;
+  [[nodiscard]] int getXw() const;
 
-    [[nodiscard]] int getXw() const;
+  [[nodiscard]] int getYw() const;
 
-    [[nodiscard]] int getYw() const;
+  [[nodiscard]] int getLevel() const;
 
-    [[nodiscard]] int getLevel() const;
+  [[nodiscard]] uint64_t getInputAddr() const;
 
-    [[nodiscard]] uint64_t getInputAddr() const;
+  [[nodiscard]] uint64_t getEdgeAddr() const;
 
-    [[nodiscard]] uint64_t getEdgeAddr() const;
+  [[nodiscard]] uint64_t getOutputAddr() const;
 
-    [[nodiscard]] uint64_t getOutputAddr() const;
+  [[nodiscard]] int getInputLen() const;
 
-    [[nodiscard]] int getInputLen() const;
+  [[nodiscard]] int getEdgeLen() const;
 
-    [[nodiscard]] int getEdgeLen() const;
+  [[nodiscard]] int getOutputLen() const;
 
-    [[nodiscard]] int getOutputLen() const;
-
-    [[nodiscard]] int getCurrentNodeSize() const;
+  [[nodiscard]] int getCurrentNodeSize() const;
 
 private:
-    int x, y, xw, yw, level;
-    uint64_t input_addr, edge_addr, output_addr;
-    int input_len, edge_len, output_len;
-    int num_nodes_in_window;
-    int current_node_size;
+  int x, y, xw, yw, level;
+  uint64_t input_addr, edge_addr, output_addr;
+  int input_len, edge_len, output_len;
+  int num_nodes_in_window;
+  int current_node_size;
+
 public:
-    [[nodiscard]] int getNumNodesInWindow() const;
+  [[nodiscard]] int getNumNodesInWindow() const;
 };
 
+template <> struct fmt::formatter<Slide_window> {
+  bool simple = false;
 
-template<>
-struct fmt::formatter<Slide_window> {
-    bool simple = false;
+  constexpr auto parse(format_parse_context &ctx) {
 
-    constexpr auto parse(format_parse_context &ctx) {
-
-
-        // Parse the presentation format and store it in the formatter:
-        auto it = ctx.begin();
-        while (it != ctx.end() and *it == 's') {
-            if (*it == 's') {
-                simple = true;
-            }
-            it++;
-        }
-        if (it != ctx.end() && *it != '}') {
-            throw format_error("invalid format");
-        }
-        return it;
+    // Parse the presentation format and store it in the formatter:
+    auto it = ctx.begin();
+    while (it != ctx.end() and *it == 's') {
+      if (*it == 's') {
+        simple = true;
+      }
+      it++;
     }
-
-
-    template<typename FormatContext>
-    auto format(const Slide_window &p, FormatContext &ctx) {
-        // auto format(const point &p, FormatContext &ctx) -> decltype(ctx.out()) // c++11
-        // ctx.out() is an output iterator to write to.
-        auto out = simple ? format_to(ctx.out(), "{} {} {} {} {}", p.getX(), p.getY(), p.getXw(), p.getYw(),
-                                      p.getLevel()) : format_to(ctx.out(),
-                                                                "x {} y {} {} {} l {} i_a {} e_a {} o_a {} i_l {} e_l {} o_l {} n_n {}",
-                                                                p.getX(),
-                                                                p.getY(), p.getXw(), p.getYw(), p.getLevel(),
-                                                                p.getInputAddr(), p.getEdgeAddr(), p.getOutputAddr(),
-                                                                p.getInputLen(), p.getEdgeLen(), p.getOutputLen(),
-                                                                p.getNumNodesInWindow());
-        return out;
+    if (it != ctx.end() && *it != '}') {
+      throw format_error("invalid format");
     }
+    return it;
+  }
+
+  template <typename FormatContext>
+  auto format(const Slide_window &p, FormatContext &ctx) {
+    // auto format(const point &p, FormatContext &ctx) -> decltype(ctx.out()) //
+    // c++11 ctx.out() is an output iterator to write to.
+    auto out =
+        simple ? format_to(ctx.out(), "{} {} {} {} {}", p.getX(), p.getY(),
+                           p.getXw(), p.getYw(), p.getLevel())
+               : format_to(ctx.out(),
+                           "x {} y {} {} {} l {} i_a {} e_a {} o_a {} i_l {} "
+                           "e_l {} o_l {} n_n {}",
+                           p.getX(), p.getY(), p.getXw(), p.getYw(),
+                           p.getLevel(), p.getInputAddr(), p.getEdgeAddr(),
+                           p.getOutputAddr(), p.getInputLen(), p.getEdgeLen(),
+                           p.getOutputLen(), p.getNumNodesInWindow());
+    return out;
+  }
 };
 
+template <typename T>
+class slide_window_set_iterator
+    : public std::iterator<std::bidirectional_iterator_tag, T> {
+
+
+
+};
 class Slide_window_set {
 public:
-    Slide_window_set(std::shared_ptr<Graph> mGraph, std::vector<int> xwS, std::vector<int> ywS,
-                     std::vector<int> nodeSizeS, int totalLevel);
+  Slide_window_set(std::shared_ptr<Graph> mGraph, std::vector<int> xwS,
+                   std::vector<int> ywS, std::vector<int> nodeSizeS,
+                   int totalLevel);
 
-    const std::vector<Slide_window> &get_windows() {
-        return m_sliding_window_vec;
-    }
+  const std::vector<Slide_window> &get_windows() {
+    return m_sliding_window_vec;
+  }
 
 private:
-    std::shared_ptr<Graph> m_graph;
-    std::vector<int> xw_s;
-    std::vector<int> yw_s;
+  std::shared_ptr<Graph> m_graph;
+  std::vector<int> xw_s;
+  std::vector<int> yw_s;
 
-    std::vector<int> node_size_s;
+  std::vector<int> node_size_s;
 
-    int total_level;
+  int total_level;
 
-    std::vector<Slide_window>
-            m_sliding_window_vec;
-    std::vector<std::vector<std::vector<Slide_window>>> m_sliding_window_multi_level;
+  std::vector<Slide_window> m_sliding_window_vec;
+  std::vector<std::vector<std::vector<Slide_window>>>
+      m_sliding_window_multi_level;
 
-    std::vector<uint64_t> node_addrs;//for each level;
-
+  std::vector<uint64_t> node_addrs; // for each level;
 };
 
-
-#endif //GCN_SIM_SLIDE_WINDOW_H
+#endif // GCN_SIM_SLIDE_WINDOW_H
