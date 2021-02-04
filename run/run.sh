021afc1e9a20d06444c6b50e1f70472f18106496
Jiangqiu Shen <jshen2@mtu.edu>
2021-02-04 06:31:53 -0500

    fix some

--- run/run.sh
+++ run/run.sh
@@ -1,6 +1,12 @@
+#./gcn_sim -input=131072 -output=4194304\
+# -edge=2097152 -agg=4194304 -aggCores=512\
+#  -systolic-rows=32 -systolic-cols=128\
+#   -graph-name=pubmed\
+#    -dram-name=HBM-config.cfg -model=gcn \
+#     -ignore-neighbor=0 -ignore-self=0
 ./gcn_sim -input=131072 -output=4194304\
  -edge=2097152 -agg=4194304 -aggCores=512\
   -systolic-rows=32 -systolic-cols=128\
    -graph-name=pubmed\
-    -dram-name=HBM-config.cfg -model=gcn \
-     -ignore-neighbor=0 -ignore-self=0
+    -dram-name=HBM2_8Gb_x128.ini -model=gcn \
+     -ignore-neighbor=0 -ignore-self=0 -use-dramsim
