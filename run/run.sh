376cef5bb0e80e7cb0f29a3863e03b022b35a2b3
Jiangqiu Shen <jshen2@mtu.edu>
2021-02-04 00:12:44 -0500

    fix bugs

--- run/run.sh
+++ run/run.sh
@@ -2,5 +2,5 @@
  -edge=2097152 -agg=4194304 -aggCores=512\
   -systolic-rows=32 -systolic-cols=128\
    -graph-name=pubmed\
-    -dram-name=HBM2_8Gb_x128.ini -model=gcn \
-     -ignore-neighbor=0 -ignore-self=0 -use-dramsim
+    -dram-name=HBM-config.cfg -model=gcn \
+     -ignore-neighbor=0 -ignore-self=0
