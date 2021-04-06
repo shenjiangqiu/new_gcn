908e7d4aa8afaf1a355f5c611f0c267e6826d14d
jyue <jyue@mtu.edu>
2021-04-06 18:20:45 -0400

    update

--- test/dram3sim_test.cc
+++ test/dram3sim_test.cc
@@ -11,18 +11,28 @@ dramsim_wrapper m_mem(config);
 int i=0;
 int cycle=0;
 int on_going=0;
-while(i<1000000){
-    if(m_mem.available()){
-        m_mem.send(i*64,false);//only read
-        i++;
-        on_going++;
-    }
+int req_cnt =  1000000;
+int upper = 1;
+while(i< req_cnt){
+    
+    for( int p = 0 ; p < upper; p++)
+        if(m_mem.available()){
+            m_mem.send(i*64,false);//only read
+            i++;
+            on_going++;
+        }
+     
     m_mem.cycle();
     cycle++;
-    if(m_mem.return_available()){
-        m_mem.pop();
-        on_going--;
-    }
+
+    for( int p = 0 ; p < upper; p++)
+        if(m_mem.return_available()){
+            m_mem.pop();
+            on_going--;
+        }
+    
+
+
 }
 
 while(on_going>0){
@@ -33,5 +43,7 @@ while(on_going>0){
         on_going--;
     }
 }
+
+std::cout<<"ramu_test BW "<< (double)64*req_cnt/(double)cycle<<" cycle "<<cycle<<"\n";
 
 }
