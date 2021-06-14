91dfdf7bb29a16a166275ef9874d3827f9644c0d
Jiangqiu Shen <jshen2@mtu.edu>
2021-06-14 09:04:07 -0400

    finished insert logic

--- src/Hash_table.h
+++ src/Hash_table.h
@@ -12,6 +12,7 @@ struct entry {
   unsigned node_id = 0;
   unsigned tag = 0;
   unsigned shift = 0;
+  unsigned total_len = 0;
 };
 
 class hash_table {
@@ -25,49 +26,103 @@ public:
     // happened
     unsigned total_cycle = 0;
     total_cycle++;
-    auto entry_id = node_id % total_entry;
-    auto &entry = m_value[entry_id];
-    if (entry.valid and entry.tag == node_id) {
-      // find it and insert it
-      auto insert_entry = entry_id + entry.shift;
 
-      auto &new_entry = m_value[insert_entry];
-      if (new_entry.valid) {
-        assert(new_entry.main);
-        auto move_cycle = move(insert_entry, entry_id);
-        total_insert_move_time += move_cycle;
-        total_cycle += move_cycle;
-      }
-      new_entry.main = false;
-      new_entry.next_valid = false;
-      m_value[insert_entry - 1].next_valid = true;
+    auto entry_id_1 = hash_func_1(node_id);
+    auto entry_id_2 = hash_func_2(node_id);
+    unsigned real_entry_id = 0;
+    bool found = false;
+    // 1, find and append
+    if (entrys.count(entry_id_1) and entrys[entry_id_1].tag == node_id) {
+      real_entry_id = entry_id_1;
+      found = true;
+    } else if (entrys.count(entry_id_2) and entrys[entry_id_2].tag == node_id) {
+      real_entry_id = entry_id_2;
+      found = true;
     } else {
-      // do not find the
-      // TODO
-      // 1. not exist
-      // 2. be moved
+      found = false;
+      // not found
+    }
+
+    if (found) {
+      auto &entry = entrys[real_entry_id];
+      auto next = entrys.upper_bound(real_entry_id);
+      if (next != entrys.end()) {
+        auto gap = next->first - real_entry_id;
+        if (gap > entry.total_len) {
+          // ok, good to go,
+          entry.total_len += 1;
+        } else {
+          // no, the space is too busy, need to move
+          move(next->first, real_entry_id, entry.total_len + 1);
+          entry.total_len += 1;
+        }
+      } else {
+        // reach the end,
+        // if fit in the space, just insert
+        if (entry.total_len + 1 + real_entry_id < total_entry) {
+          entry.total_len += 1;
+        } else {
+          // try to insert to the other side// from zero
+
+          // FIXBUG here, the base shift is need to add
+          auto begin_size = total_entry - entry.total_len - 1 - real_entry_id;
+          assert(begin_size > 0);
+          auto begin_it = entrys.begin();
+          assert(begin_it != entrys.end());
+          if (begin_it->first - begin_size <= 0) {
+            // need to move
+            move(begin_it->first, real_entry_id, entry.total_len + 1);
+          } else {
+            entry.total_len += 1;
+          }
+        }
+      }
+    } else {
+      // not found, need to new an entry
+      if (is_entry_empty(entry_id_1, 1)) {
+        // choose, entry 1
+        auto t_entry = entry();
+        t_entry.tag = node_id;
+        t_entry.total_len = 1;
+        entrys[entry_id_1] = t_entry;
+      } else if (is_entry_empty(entry_id_2, 1)) {
+        auto t_entry = entry();
+        t_entry.tag = node_id;
+        t_entry.total_len = 1;
+        entrys[entry_id_2] = t_entry;
+      } else {
+        // both are not empty, find a shortest one to move
+        auto shortest = find_shortest_to_move(entry_id_1, entry_id_2);
+        move(shortest, shortest, 1);
+        auto t_entry = entry();
+        t_entry.tag = node_id;
+        t_entry.total_len = 1;
+        entrys[shortest] = t_entry;
+      }
     }
   }
   //
   unsigned query(unsigned node_id) {
     // currently we assume no conflict and return the number of entries
-    return functional_number_of_entries.at(node_id);
+    // dont' worry, it's constant
+    auto entryId = find_real_entry_id(node_id);
+    auto entry_size = entrys[entryId].total_len;
+    entrys.erase(entryId);
+    // T
+    return entry_size;
 
-    // TODO, might be moved!
-  }
-  unsigned invalid(unsigned node_id) {
-    // currently we assume no conflict, just invalid all entries
-    auto result = functional_number_of_entries.at(node_id);
-    functional_number_of_entries.erase(node_id);
-    return result;
   }
 
 private:
-  unsigned move(unsigned entry_id, unsigned on_fail_node_id) {
-    // todo implemnt it!
+  unsigned move(unsigned entry_id, unsigned on_fail_node_id, unsigned len) {
+    // TODO implemnt it!
+
     return 0;
   }
   std::map<unsigned, unsigned> functional_number_of_entries;
+
+  std::map<unsigned, entry> entrys;
+
   unsigned total_entry;
 
   unsigned total_search_time{0};
@@ -75,6 +130,26 @@ private:
   unsigned total_insert_move_time{0};
 
   unsigned total_invalid_move_time{0};
+
+  unsigned hash_func_1(unsigned node_id) {
+    // TODO
+  }
+  unsigned hash_func_2(unsigned node_id) {
+    // TODO
+  }
+  bool is_entry_empty(unsigned entry_id, unsigned size) {
+    // TODO
+  }
+  unsigned find_real_entry_id(unsigned node_id) {
+    // TODO
+  }
+  unsigned find_shortest_to_move(unsigned entryId1, unsigned entryId2) {
+    // TODO
+  }
+  unsigned get_real_entry_id_by_inner_id(unsigned entryId) {
+    // TODO
+  }
+
   std::vector<entry> m_value;
 };
 
